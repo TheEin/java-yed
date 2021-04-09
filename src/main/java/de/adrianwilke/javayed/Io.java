a38f2830a7322b06c895eb4bdc18322c72e4f8e4
Alexandr Kuramshin <Alexandr_Kuramshin@epam.com>
2021-04-09 15:41:26 +0700

    Extend Io API

--- src/main/java/de/adrianwilke/javayed/Io.java
+++ src/main/java/de/adrianwilke/javayed/Io.java
@@ -3,8 +3,11 @@ package de.adrianwilke.javayed;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,14 +22,24 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-public abstract class Io {
+public class Io {
+
+	private Io() {
+	}
 
 	/**
 	 * Writes XML document to file.
 	 */
 	public static void write(Document document, File file) throws IOException, TransformerException {
+		write(document, new FileOutputStream(file));
+	}
+
+	/**
+	 * Writes XML document to OutputStream.
+	 */
+	public static void write(Document document, OutputStream out) throws IOException, TransformerException {
 		DOMSource domSource = new DOMSource(document);
-		StreamResult streamResult = new StreamResult(new FileWriter(file));
+		StreamResult streamResult = new StreamResult(new OutputStreamWriter(out, StandardCharsets.UTF_8));
 
 		Transformer transformer = TransformerFactory.newInstance().newTransformer();
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
